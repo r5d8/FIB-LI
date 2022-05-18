e7985da7b166e0ba03071d468e55b1de12bae73f
pol.sturlese <pol.sturlese@estudiantat.upc.edu>
2022-05-18 09:50:19 +0200

    letterDice

--- L06/letterDice.pl
+++ L06/letterDice.pl
@@ -37,10 +37,22 @@ main:-
     
     %Constraints
     append(D1, D2, D12), append(D3, D4, D34), append(D12, D34, Dtot),
-    alldifferent(Dtot),
+    all_different(Dtot),
+    
+    %word(W),
+    %createsWord(W, [D1, D2, D3, D4]),
+    
+    parejasDistintas(P),
+    incompatibles_en_dados(P, D1),
+    incompatibles_en_dados(P, D2),
+    incompatibles_en_dados(P, D3),
+    incompatibles_en_dados(P, D4),
+    
+    romper_simetria_1(D1, D2),
+    sortedOK(D1), sortedOK(D2), sortedOK(D3), sortedOK(D4), 
     
     %solve
-    lable(Dtot),
+    labeling([ff], Dtot),
     
     %Write sol
     writeN(D1), 
@@ -49,3 +61,24 @@ main:-
     writeN(D4), halt.
     
 writeN(D):- findall(X,(member(N,D),num(X,N)),L), write(L), nl, !.
+
+%createsWord([],[]).
+%createsWord([L|W], Dices) :- append(P1, [D|P2], Dices), member(Face, D), Face #= L, append(P1, P2, P), createsWord(W, P).
+
+parejasDistintas(L) :- findall(NA-NB, (word(W), member(A, W), member(B, W), num(A, NA), num(B, NB), NA < NB), L).
+
+%romper_simetria_1([A|_], [B|_], [C|_], [D|_]) :- A #= 2, B #= 1, C #= 11, D #= 5. %b, a, k, e
+romper_simetria_1([A|_], [B|_]) :- A #= 2, B #= 1. %b, a. k, e no les poso pq potser no estarà sorted sinó!!
+
+%Es pot trencar simetria fent tb que estigui sorted
+sortedOK([_]).                                          %%%% OK!
+sortedOK([X,Y|L]):- X #< Y, sortedOK([Y|L]).
+
+incompatibles_en_dados([], _).
+incompatibles_en_dados([N-M|Pl], [A,B,C,D,E,F]) :- 
+    incompatibles_en_dados(Pl, [A,B,C,D,E,F]),
+    A #\= N #\/ B #\= M, A #\= N #\/ C #\= M, A #\= N #\/ D #\= M, A #\= N #\/ E #\= M, A #\= N #\/ F #\= M,
+                         B #\= N #\/ C #\= M, B #\= N #\/ D #\= M, B #\= N #\/ E #\= M, B #\= N #\/ F #\= M,
+                                              C #\= N #\/ D #\= M, C #\= N #\/ E #\= M, C #\= N #\/ F #\= M,
+                                                                   D #\= N #\/ E #\= M, D #\= N #\/ F #\= M,
+                                                                                        E #\= N #\/ F #\= M.
